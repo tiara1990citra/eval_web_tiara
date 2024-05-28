910e1682cb12a9d403b4b93a2293280494e0fa5c
tiara1990citra <tiara1990citra@gmail.com>
2024-05-28 20:07:20 +0700

    b

--- login.php
+++ login.php
@@ -1,3 +1,24 @@
+<?php 
+session_start();
+include("assets/database.php");
+if(isset(($_POST['login']))){
+  $username = $_POST['login'];
+  $password = $_POST['password'];
+  $query = 'select * from admin query username =$usernama';
+  $result = mysqli_query($conn, $query);
+  if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_array($result);
+    $username = $row['username'];
+    $password = $row['password'];
+  
+    
+
+  
+}
+
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +37,7 @@
       
       <label for="password">Password:</label>
       <input type="password" id="password" name="password" required autocomplete="on">
-      <button type="submit">Login</button>
+      <button type="submit" name="login">Login</button>
     
     </form>
   </div>
