9734fed56f90cfee456d639afc4c67eacddec29e
tiara1990citra <tiara1990citra@gmail.com>
2024-05-28 19:18:44 +0700

    login

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,9 @@
-<?php include('index.php')?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="assets/login.css">
 </head>
 <body>
     <div class="container">
